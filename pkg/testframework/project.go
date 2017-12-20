72b54bf5e6fe0a15b268d50f0831cba20842b943
Oleg Bulatov <obulatov@redhat.com>
2017-12-20 16:02:56 +0100

    Fix crossmount test

--- pkg/testframework/project.go
+++ pkg/testframework/project.go
@@ -1,6 +1,8 @@
 package testframework
 
 import (
+	"testing"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	kapi "k8s.io/kubernetes/pkg/api/v1"
@@ -11,7 +13,7 @@ import (
 	projectv1 "github.com/openshift/origin/pkg/project/generated/clientset/typed/project/v1"
 )
 
-func CreateProject(clientConfig *rest.Config, namespace string, adminUser string) (*projectapiv1.Project, error) {
+func CreateProject(t *testing.T, clientConfig *rest.Config, namespace string, adminUser string) *projectapiv1.Project {
 	projectClient := projectv1.NewForConfigOrDie(clientConfig)
 	project, err := projectClient.ProjectRequests().Create(&projectapiv1.ProjectRequest{
 		ObjectMeta: metav1.ObjectMeta{
@@ -19,7 +21,7 @@ func CreateProject(clientConfig *rest.Config, namespace string, adminUser string
 		},
 	})
 	if err != nil {
-		return project, err
+		t.Fatal(err)
 	}
 
 	authorizationClient := authorizationv1.NewForConfigOrDie(clientConfig)
@@ -32,5 +34,19 @@ func CreateProject(clientConfig *rest.Config, namespace string, adminUser string
 			Name: "admin",
 		},
 	})
-	return project, err
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("project %s is created", project.Name)
+
+	return project
+}
+
+func DeleteProject(t *testing.T, clientConfig *rest.Config, name string) {
+	projectClient := projectv1.NewForConfigOrDie(clientConfig)
+	if err := projectClient.Projects().Delete(name, &metav1.DeleteOptions{}); err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("project %s is deleted", name)
 }
