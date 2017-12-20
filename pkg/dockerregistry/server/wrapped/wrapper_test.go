fc45d12b558c7f64dbc6970039df411cd561c577
Oleg Bulatov <obulatov@redhat.com>
2017-12-20 15:54:25 +0100

    Add wrapped.NewBlobDescriptorService

--- pkg/dockerregistry/server/wrapped/wrapper_test.go
+++ pkg/dockerregistry/server/wrapped/wrapper_test.go
@@ -22,12 +22,6 @@ func zeroIn(typ reflect.Type) []reflect.Value {
 }
 
 func TestWrapper(t *testing.T) {
-	var lastFuncName string
-	captureFuncName := func(ctx context.Context, funcname string, f func(ctx context.Context) error) error {
-		lastFuncName = funcname
-		return fmt.Errorf("don't call upstream code")
-	}
-
 	exclude := map[string]bool{
 		"BlobWriter.Close":     true,
 		"BlobWriter.ID":        true,
@@ -37,13 +31,20 @@ func TestWrapper(t *testing.T) {
 		"BlobWriter.Write":     true,
 	}
 
-	wrappers := []reflect.Value{
+	// check funcname
+	var lastFuncName string
+	captureFuncName := func(ctx context.Context, funcname string, f func(ctx context.Context) error) error {
+		lastFuncName = funcname
+		return fmt.Errorf("don't call upstream code")
+	}
+	objects := []reflect.Value{
 		reflect.ValueOf(&blobStore{wrapper: captureFuncName}),
-		reflect.ValueOf(&blobWriter{wrapper: captureFuncName}),
 		reflect.ValueOf(&manifestService{wrapper: captureFuncName}),
 		reflect.ValueOf(&tagService{wrapper: captureFuncName}),
+		reflect.ValueOf(&blobWriter{wrapper: captureFuncName}),
+		reflect.ValueOf(&blobDescriptorService{wrapper: captureFuncName}),
 	}
-	for _, v := range wrappers {
+	for _, v := range objects {
 		typeName := strings.Title(v.Elem().Type().Name())
 		for i := 0; i < v.Type().NumMethod(); i++ {
 			lastFuncName = "unhandled"
@@ -67,7 +68,64 @@ func TestWrapper(t *testing.T) {
 			}
 
 			if lastFuncName != expectedFuncName {
-				t.Errorf("%s: got %q, want %q", funcName, lastFuncName, expectedFuncName)
+				t.Errorf("%s: got funcname %q, want %q", funcName, lastFuncName, expectedFuncName)
+			}
+		}
+	}
+
+	// check calls chain
+	var lastResult struct {
+		c   int
+		err error
+	}
+	checkWrapper := func(ctx context.Context, funcname string, f func(ctx context.Context) error) error {
+		err := f(ctx)
+		lastResult.c++
+		if err == nil {
+			lastResult.err = fmt.Errorf("got call of unknown method, want %q", funcname)
+		} else if err.Error() != funcname {
+			lastResult.err = fmt.Errorf("got call of %q, want %q", err, funcname)
+		}
+		return nil
+	}
+	dummyWrapper := func(ctx context.Context, funcname string, f func(ctx context.Context) error) error {
+		return fmt.Errorf("%s", funcname)
+	}
+	objects = []reflect.Value{
+		reflect.ValueOf(NewBlobStore(NewBlobStore(nil, dummyWrapper), checkWrapper)),
+		reflect.ValueOf(NewManifestService(NewManifestService(nil, dummyWrapper), checkWrapper)),
+		reflect.ValueOf(NewTagService(NewTagService(nil, dummyWrapper), checkWrapper)),
+		reflect.ValueOf(NewBlobWriter(NewBlobWriter(nil, dummyWrapper), checkWrapper)),
+		reflect.ValueOf(NewBlobDescriptorService(NewBlobDescriptorService(nil, dummyWrapper), checkWrapper)),
+	}
+	for _, v := range objects {
+		typeName := strings.Title(v.Elem().Type().Name())
+		for i := 0; i < v.Type().NumMethod(); i++ {
+			lastResult.c = 0
+			lastResult.err = nil
+
+			methodName := v.Type().Method(i).Name
+			funcName := fmt.Sprintf("%s.%s", typeName, methodName)
+
+			if exclude[funcName] {
+				continue
+			}
+
+			method := v.Method(i)
+			args := zeroIn(method.Type())
+			func() {
+				defer func() {
+					// BlobWriter.Close and other unhandled methods may panic
+					recover()
+				}()
+				method.Call(args)
+			}()
+
+			if lastResult.c != 1 {
+				t.Errorf("%s: got %d calls, want 1", funcName, lastResult.c)
+			}
+			if lastResult.c > 0 && lastResult.err != nil {
+				t.Errorf("%s: %s", funcName, lastResult.err)
 			}
 		}
 	}
