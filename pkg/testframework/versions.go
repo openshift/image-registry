744579f74014e9b38f2a4bd20eae7b23763a05c7
Ben Parees <bparees@redhat.com>
2018-01-29 18:24:25 -0500

    switch back to using latest origin version

--- pkg/testframework/versions.go
+++ pkg/testframework/versions.go
@@ -1,4 +1,4 @@
 package testframework
 
 // originImageRef is the Docker image name of the OpenShift Origin container.
-const originImageRef = "docker.io/openshift/origin:v3.9.0-alpha.2"
+const originImageRef = "docker.io/openshift/origin:latest"
