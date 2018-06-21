openshift/docker-distribution

UPSTREAM: docker/distribution: 2580: Allow to use custom cache provider
Signed-off-by: Gladkov Alexey <agladkov@redhat.com>

Image-registry-commit: 86e42f037aaf3e33230f6c5146cd3b9a5a841c45

registry/handlers/app.go
@@ -31,6 +31,7 @@ import (
 	"github.com/docker/distribution/registry/proxy"
 	"github.com/docker/distribution/registry/storage"
 	memorycache "github.com/docker/distribution/registry/storage/cache/memory"
+	cacheprovider "github.com/docker/distribution/registry/storage/cache/provider"
 	rediscache "github.com/docker/distribution/registry/storage/cache/redis"
 	storagedriver "github.com/docker/distribution/registry/storage/driver"
 	"github.com/docker/distribution/registry/storage/driver/factory"
@@ -287,7 +288,20 @@ func NewApp(ctx context.Context, config *configuration.Configuration) *App {
 			dcontext.GetLogger(app).Infof("using inmemory blob descriptor cache")
 		default:
 			if v != "" {
-				dcontext.GetLogger(app).Warnf("unknown cache type %q, caching disabled", config.Storage["cache"])
+				name, ok := v.(string)
+				if !ok {
+					panic(fmt.Sprintf("unexpected type of value %T (string expected)", v))
+				}
+				cacheProvider, err := cacheprovider.Get(app, name, cc)
+				if err != nil {
+					panic("unable to initialize cache provider: " + err.Error())
+				}
+				localOptions := append(options, storage.BlobDescriptorCacheProvider(cacheProvider))
+				app.registry, err = storage.NewRegistry(app, app.driver, localOptions...)
+				if err != nil {
+					panic("could not create registry: " + err.Error())
+				}
+				dcontext.GetLogger(app).Infof("using %s blob descriptor cache", name)
 			}
 		}
 	}

openshift/image-registry

vendor/github.com/docker/distribution/registry/handlers/app.go
@@ -30,6 +30,7 @@ import (
 	"github.com/docker/distribution/registry/proxy"
 	"github.com/docker/distribution/registry/storage"
 	memorycache "github.com/docker/distribution/registry/storage/cache/memory"
+	cacheprovider "github.com/docker/distribution/registry/storage/cache/provider"
 	rediscache "github.com/docker/distribution/registry/storage/cache/redis"
 	storagedriver "github.com/docker/distribution/registry/storage/driver"
 	"github.com/docker/distribution/registry/storage/driver/factory"
@@ -276,7 +277,20 @@ func NewApp(ctx context.Context, config *configuration.Configuration) *App {
 			ctxu.GetLogger(app).Infof("using inmemory blob descriptor cache")
 		default:
 			if v != "" {
-				ctxu.GetLogger(app).Warnf("unknown cache type %q, caching disabled", config.Storage["cache"])
+				name, ok := v.(string)
+				if !ok {
+					panic(fmt.Sprintf("unexpected type of value %T (string expected)", v))
+				}
+				cacheProvider, err := cacheprovider.Get(app, name, cc)
+				if err != nil {
+					panic("unable to initialize cache provider: " + err.Error())
+				}
+				localOptions := append(options, storage.BlobDescriptorCacheProvider(cacheProvider))
+				app.registry, err = storage.NewRegistry(app, app.driver, localOptions...)
+				if err != nil {
+					panic("could not create registry: " + err.Error())
+				}
+				ctxu.GetLogger(app).Infof("using %s blob descriptor cache", name)
 			}
 		}
 	}