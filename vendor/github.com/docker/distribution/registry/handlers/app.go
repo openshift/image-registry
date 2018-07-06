openshift/docker-distribution

UPSTREAM: docker/distribution: <carry>: Log failed HEAD requests as a info
Signed-off-by: Gladkov Alexey <agladkov@redhat.com>

Image-registry-commit: 7c4b4e458e5dc3983b637d9cfe4f8170e5d328a8

registry/handlers/app.go
@@ -809,7 +809,18 @@ func (app *App) logError(ctx context.Context, errors errcode.Errors) {
 			errCodeKey{},
 			errMessageKey{},
 			errDetailKey{}))
-		dcontext.GetResponseLogger(c).Errorf("response completed with error")
+
+		logf := dcontext.GetResponseLogger(c).Errorf
+
+		httpStatus, ok := c.Value("http.response.status").(int)
+		if ok && httpStatus == 404 {
+			httpMethod, ok := c.Value("http.request.method").(string)
+			if ok && strings.ToLower(httpMethod) == "head" {
+				logf = dcontext.GetResponseLogger(c).Infof
+			}
+		}
+
+		logf("response completed with error")
 	}
 }
 

openshift/image-registry

vendor/github.com/docker/distribution/registry/handlers/app.go
@@ -779,7 +779,18 @@ func (app *App) logError(context context.Context, errors errcode.Errors) {
 			errCodeKey{},
 			errMessageKey{},
 			errDetailKey{}))
-		ctxu.GetResponseLogger(c).Errorf("response completed with error")
+
+		logf := ctxu.GetResponseLogger(c).Errorf
+
+		httpStatus, ok := c.Value("http.response.status").(int)
+		if ok && httpStatus == 404 {
+			httpMethod, ok := c.Value("http.request.method").(string)
+			if ok && strings.ToLower(httpMethod) == "head" {
+				logf = ctxu.GetResponseLogger(c).Infof
+			}
+		}
+
+		logf("response completed with error")
 	}
 }
 