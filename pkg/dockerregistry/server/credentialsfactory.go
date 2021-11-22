46ae9442e427b4fd183bf51386640091504e9c66
Oleg Bulatov <oleg@bulatov.me>
2021-11-22 14:27:37 +0100

    Fix auth for docker.io images

--- pkg/dockerregistry/server/credentialsfactory.go
+++ pkg/dockerregistry/server/credentialsfactory.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"strings"
+
 	"github.com/docker/distribution/registry/client/auth"
 	dockerregistry "github.com/docker/docker/registry"
 	"github.com/openshift/library-go/pkg/image/registryclient"
@@ -20,6 +22,10 @@ func (c *credentialStoreFactory) CredentialStoreFor(image string) auth.Credentia
 	var nocreds auth.CredentialStore = registryclient.NoCredentials
 	if c.keyring == nil {
 		return nocreds
+	}
+
+	if strings.HasPrefix(image, "registry-1.docker.io/") {
+		image = image[len("registry-1."):]
 	}
 
 	auths, _ := c.keyring.Lookup(image)
